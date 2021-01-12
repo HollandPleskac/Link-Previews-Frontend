4f85a3ddcacb4c7b4eedb9c5e2fe9b675ad0c683
Holland Pleskac <hollandpleskac@gmail.com>
2021-01-11 17:26:26 -0800

    displaying data from the server

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-const previewDiv = document.getElementById('preview-div');
+const previewList = document.getElementById('preview-list');
 const submitBtn = document.getElementById('submit-btn');
+const textarea = document.querySelector('textarea');
 
 submitBtn.addEventListener('click', async function () {
+  const textContent = textarea.value;
   const response = await fetch('http://localhost:3000/',
     {
       method: 'POST',
-      body: JSON.stringify({text:'https://www.amazon.com/ is the amazons website'}),
+      body: JSON.stringify({ text: textContent }),
       headers: {
         'Content-Type': 'application/json'
       }
     }).then(response => {
       return response.json();
     });
-    console.log(response.data);
+  console.log(response.data);
+
+  for (i = 0; i < response.data.length; i++) {
+    previewList.insertAdjacentHTML(
+      'beforeend',
+      `<div class="preview-div">
+        <img src="${response.data[i].image || './img/img_not_found.svg'}"
+          alt="${response.data[i].title}">
+        <div class="preview-content" style="background-color:${response.data[i].themeColor === "#000000" ? "#fff" : response.data[i].themeColor}">
+          <h2>${response.data[i].title}</h2>
+          <p>${response.data[i].description}</p>
+        </div>
+      </div>`
+    );
+  }
+
 });
